c6cc39175b2c87c8b931eb70a4c69664d431c89e
Colyn <colynjonker@hotmail.com>
2022-11-24 16:14:14 +0100

    Updates

--- medusa-config.js
+++ medusa-config.js
@@ -54,7 +54,7 @@ const plugins = [
 		options:
 			{
 				// The baseurl for your medusajs server
-				serverBaseUrl: "http://localhost:9000",
+				serverBaseUrl: "http://localhost:9001",
 				// when enabled saves the file as a base64 encoded string inside the database (deleting that row is not yet supported)
 				saveInDatabase: false, // recommended: false
 				// the folder where your files are stored on the server
